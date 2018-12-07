7e4b42a337473b5c4a7e5244711f89e924d49852
Serwis <andjakulbn@gmail.com>
2018-12-07 20:21:23 +0100

    change cucumber

--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -1,12 +1,15 @@
 package pl.lait.selenium1;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 	static WebDriver driver;
@@ -15,9 +18,16 @@ public class Init {
 	public static WebDriver getDriver() {
 		System.setProperty("webdriver.chrome.driver", "d:\\tester\\chromedriver_win32\\chromedriver.exe");
     	DesiredCapabilities cap = DesiredCapabilities.chrome();
-    	
+    	URL url = null;
+		try {
+			url = new URL("http://192.168.0.125:4444/wd/hub");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	if (driver==null) {
-    			driver = new  ChromeDriver();
+    			//driver = new  ChromeDriver();
+    			driver = new RemoteWebDriver(url, cap);
     	driver.get("http://newtours.demoaut.com");
     	//driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);  timeout dla całęj strony
     	return driver;
