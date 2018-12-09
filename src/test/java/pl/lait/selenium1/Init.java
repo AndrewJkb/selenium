cc2557b935914e4e6edf7d967885d553c159feef
Serwis <andjakulbn@gmail.com>
2018-12-09 09:23:30 +0100

    Add print screen

--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -1,13 +1,20 @@
 package pl.lait.selenium1;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -18,16 +25,16 @@ public class Init {
 	public static WebDriver getDriver() {
 		System.setProperty("webdriver.chrome.driver", "d:\\tester\\chromedriver_win32\\chromedriver.exe");
     	DesiredCapabilities cap = DesiredCapabilities.chrome();
-    	URL url = null;
+    	/*URL url = null;
 		try {
 			url = new URL("http://192.168.0.125:4444/wd/hub");
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
     	if (driver==null) {
-    			//driver = new  ChromeDriver();
-    			driver = new RemoteWebDriver(url, cap);
+    			driver = new  ChromeDriver();
+    			//driver = new RemoteWebDriver(url, cap);
     	driver.get("http://newtours.demoaut.com");
     	//driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);  timeout dla całęj strony
     	return driver;
@@ -54,6 +61,22 @@ public class Init {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void printScr(WebDriver driver, String folderName,String testName) {
+		//robienie screenów
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		Long milis = timestamp.getTime();
+
+		WebDriver driver_tmp = new Augmenter().augment(driver);
+		File srcFile = ((TakesScreenshot)driver_tmp).getScreenshotAs(OutputType.FILE);
+
+		try {
+		FileUtils.copyFile(srcFile, new File("target/" + folderName + "/" + testName + "-" + "screenshot-" + milis +".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	
 	
 	
 }
--- src/test/java/pl/lait/selenium1/Login2Test.java
+++ src/test/java/pl/lait/selenium1/Login2Test.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 
 import pl.lait.pageObject.LoginPage;
 import pl.lait.pageObject.ReservationPage;
+import pl.lait.selenium1.Init;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Login2Test {
@@ -37,40 +38,57 @@ WebDriver driver = null;
 		loginPage.goToLoginPage();
 		loginPage.loginAs("tester129", "qwerty123");
 		
-		
+		String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		ReservationPage reservationPage = new ReservationPage();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.roundTripRadioClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.passengersCount("1");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.passengersCount("2");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.passengersCount("3");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.passengersCount("4");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.passengersCount("2");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Frankfurt");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("London");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("New York");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Paris");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Portland");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("San Francisco");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Seattle");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Sydney");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("Zurich");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.fromPortSelect("London");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		// wybieranie miesiaca
 		
@@ -78,55 +96,77 @@ WebDriver driver = null;
 		
 		for (int i = 1; i <= 12; i++) {
 		reservationPage.fromMonthSelect(i);
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		}
 		
 		//today.getMonth();
 		
 		reservationPage.fromDaySelect("26");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		
 		reservationPage.toPortSelect("New York");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.toMonthSelect("11");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.toDaySelect("30");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.EconomyClassRadioClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.airlineSelect("No Preference");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.findFlightsClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.outflightClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.inflightClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.reserveFlightsClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.firstPerson("Janusz", "Kowlaski");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.pass0mealSelect("LCML");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.SecondPerson("Janina", "Kowalski","VGML");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		//reservationPage.pass1mealSelect("VGML");
 		//Init.sleep(1);
 		reservationPage.creditCard("BA");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.creditnumber("2134132423");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.cc_exp_dt_mn("01");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.cc_exp_dt_yr("2010");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.cc_person("Janusz", "John", "Kowalski");
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.SameAsBillingAddressCheckboxClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(1);
 		reservationPage.buyFlightsClick();
+		Init.printScr(driver, "Login2test", testName);
 		//Init.sleep(3);
 		reservationPage.LogoutButtonClick();
+		Init.printScr(driver, "Login2test", testName);
 		Init.sleep(2);
 	}
 	
